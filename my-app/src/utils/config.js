053e6f6da869cc1695d67e2bf42cd5968cb54f2c
Pranav k <pranavkkeloth@gmail.com>
2024-12-20 15:10:05 +0530

    feat: url up

--- my-app/src/utils/config.js
+++ my-app/src/utils/config.js
@@ -1,6 +1,7 @@
 // src/config.js
 const config = {
-  API_URL: "http://localhost:5000/api", // Store the API URL here
+  // API_URL: "http://localhost:5000/api",
+  API_URL: "https://quiz-app-backend-production-a0ad.up.railway.app/api", // Store the API URL here
 };
 
 export default config;
